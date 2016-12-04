540674f2085076343a16ef48eee195d08830a426
Vasiliy Goryachev <glay95g@gmail.com>
2016-12-04 22:25:54 +0400

    some shit

--- src/model/P.java
+++ src/model/P.java
@@ -10,18 +10,27 @@ public class P extends Function {
         super(t, 2 * Math.PI * R);
     }
 
+    private double getA(int k) {
+        return 2.0 * R * (2.0 * sin(k * PI / 4.0) - sin(k * PI / 2.0));
+    }
+
+    private double getB(int k) {
+        return 2.0 * R * (2.0 * cos(k * PI / 4.0) + cos(k * PI / 2.0) - 1.0);
+    }
+
+    private double a0 = R * (0.125 + (PI - 1.0) / (2.0 * PI * R));
+
     @Override
     public double getValue(double x) {
         int n = 100;
         double result = 0.0;
         for (int k = 1; k < n; k++) {
             double sum = 0.0;
-            sum += 2.0 * R * (2.0 * sin(k * PI / 4.0) - sin(k * PI / 2.0)) * sin(k * x / (2.0 * R));
-            sum += 2.0 * R * (2.0 * cos(k * PI / 4.0) + cos(k * PI / 2.0) - 1.0) * cos(k * x / (2.0 * R));
+            sum += 2.0 * R * getA(k) * sin(k * x / (2.0 * R));
+            sum += 2.0 * R * getB(k) * cos(k * x / (2.0 * R));
             result += sum * (1.0 / (k * k)) * exp(-t * (k * A / (2.0 * R)) * (k * A / (2.0 * R)));
         }
         result *= (4.0 / (PI * PI));
-        double a0 = R * (0.125 + (PI - 1.0) / (2.0 * PI * R));
         return result + a0 / 2.0;
     }
 }
